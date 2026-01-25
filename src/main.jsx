90a58b2d4e90b25c1c2f6c0139b888ab84ec259d
rocketmike12 <mg.korop@gmail.com>
2026-01-25 19:04:17 +0200

    replaced BrowserRouter with HashRouter to fix pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<BrowserRouter basename="/goiteens_fe6_hw25/">
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</StrictMode>
 );
