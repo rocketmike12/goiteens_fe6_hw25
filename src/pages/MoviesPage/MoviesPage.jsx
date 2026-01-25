0ad4ed9e60d4b32e03c70c2793de745c3d169989
rocketmike12 <mg.korop@gmail.com>
2026-01-25 19:17:13 +0200

    search params fix; movie poster filtration

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -10,15 +10,14 @@ import { Header } from "../../components/Header/Header";
 import { MovieCard } from "../../components/MovieCard/MovieCard";
 
 export const MoviesPage = function () {
-	const [query, setQuery] = useState("");
 	const [movies, setMovies] = useState([]);
 
 	const [searchParams, setSearchParams] = useSearchParams();
 
 	const getData = async function () {
 		try {
-			const res = await tmdbApi.get("/search/movie", { params: { query: query } });
-			setMovies(res.data.results);
+			const res = await tmdbApi.get("/search/movie", { params: { query: searchParams.get("query") } });
+			setMovies(res.data.results.filter((el) => el.poster_path));
 			console.log(res);
 		} catch (err) {
 			console.error(err);
@@ -26,19 +25,21 @@ export const MoviesPage = function () {
 	};
 
 	useEffect(() => {
-		setQuery(searchParams.get("query"));
-	}, []);
-
-	useEffect(() => {
 		getData();
-	}, [query]);
+	}, [searchParams]);
+
+	const handleSearch = function (e) {
+		e.preventDefault();
+		const form = e.currentTarget;
+		setSearchParams({ query: form.elements.query.value });
+	};
 
 	return (
 		<>
 			<Header />
 			<Container>
 				<h1>SEARCH MOVIES</h1>
-				<form>
+				<form onSubmit={handleSearch}>
 					<input type="text" name="query" placeholder="search movies" />
 					<button type="submit">search</button>
 				</form>
