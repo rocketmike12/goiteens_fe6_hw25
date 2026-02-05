df6366b0e86a8572eb2b499746a85f7dfac952c4
rocketmike12 <mg.korop@gmail.com>
2026-02-05 20:28:19 +0200

    MovieCard fonts and trimming update

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 import styles from "./MovieCard.module.scss";
 
 export const MovieCard = function ({ title, overview, poster, movieId }) {
+	const trimText = (text, lim) => text.slice(0, lim - 3).split(" ").slice(0, -1).reduce((acc, val) => acc + " " + val, "") + "...";
+
 	return (
 		<>
 			<li className={styles["movie-card"]}>
 				<Link to={`/movies/${movieId}`} className={styles["movie-card__link"]}>
 					<img className={styles["movie-card__poster"]} src={`https://image.tmdb.org/t/p/w500/${poster}`} alt={`poster for ${title}`} />
 					<h3 className={styles["movie-card__title"]}>{title}</h3>
-					<p className={styles["movie-card__overview"]}>{overview.slice(0, 128) + "..."}</p>
+					<p className={styles["movie-card__overview"]}>{trimText(overview, 70)}</p>
 				</Link>
 			</li>
 		</>
