cd1abb8f694e7d925c72e68bbad24105114956a4
rocketmike12 <mg.korop@gmail.com>
2026-02-06 19:17:12 +0200

    MovieDetails styles done

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -11,6 +11,8 @@ import { Reviews } from "./Reviews/Reviews.jsx";
 import { Container } from "../../components/Container/Container";
 import { Header } from "../../components/Header/Header";
 
+import styles from "./MovieDetailsPage.module.scss";
+
 export const MovieDetailsPage = function ({ cast, reviews }) {
 	const params = useParams();
 
@@ -32,14 +34,22 @@ export const MovieDetailsPage = function ({ cast, reviews }) {
 	return (
 		<>
 			<Header />
-			<section>
+			<section className={styles["movie-details"]}>
 				<Container>
-					<h1>{movieData.title}</h1>
-					<img src={`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`} alt={`poster for ${movieData.title}`} />
-					<p>{movieData.overview}</p>
+					<img src={`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`} alt={`poster for ${movieData.title}`} className={styles["movie-details__img"]} />
 
-					<Link to={`/movies/${params.movieId}/cast`}>Cast</Link>
-					<Link to={`/movies/${params.movieId}/reviews`}>Reviews</Link>
+					<div className={styles["movie-details__content"]}>
+						<h1 className={styles["movie-details__content__title"]}>{movieData.title}</h1>
+						<p className={styles["movie-details__content__overview"]}>{movieData.overview}</p>
+						<div className={styles["movie-details__content__link-wrap"]}>
+							<Link to={`/movies/${params.movieId}/cast`} className={styles["movie-details__content__link"]}>
+								Cast
+							</Link>
+							<Link to={`/movies/${params.movieId}/reviews`} className={styles["movie-details__content__link"]}>
+								Reviews
+							</Link>
+						</div>
+					</div>
 				</Container>
 			</section>
 
